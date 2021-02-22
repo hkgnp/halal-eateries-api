c79a1c3de6658bcfda340bbf9fb390386e6320ab
hkgnp <benpng@u.nus.edu>
2021-02-23 00:48:35 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const MongoUtil = require('./MongoUtil');
+const mongoUrl = process.env.MONGO_URL;
+
+// Add in the missing requires for the API to work
+const cors = require('cors');
+
+// create an instance of express app
+let app = express();
+
+// Add in the missing express.use() for the API to work
+app.use(express.json());
+app.use(cors());
+
+async function main() {
+  const DBNAME = 'halal_eateries';
+  let db = await MongoUtil.connect(mongoUrl, DBNAME);
+
+  // GET
+  try {
+    app.get('/referrals', async (req, res) => {
+      let result = await db.collection('details').find({}).toArray();
+      res.send(result);
+    });
+  } catch (e) {
+    res.send(
+      `Apologies. API was not consumed successfully. Please contact the administrator at Github`
+    );
+    console.log(e);
+  }
+}
+
+main();
+
+app.listen(process.env.PORT || 3000, () => console.log('Server is running...'));
